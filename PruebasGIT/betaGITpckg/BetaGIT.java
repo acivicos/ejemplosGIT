f12ca118b3da748b20f8fc26efbd618aecacc627
casa <casa@192.168.0.193>
2013-04-23 22:05:42 +0200

    Esta es una tercera versión con un error.

--- PruebasGIT/betaGITpckg/BetaGIT.java
+++ PruebasGIT/betaGITpckg/BetaGIT.java
@@ -7,7 +7,7 @@ public class BetaGIT {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("Hola Mundo");
+			System.outprint("Hola Mundo");
 
 	}
 
